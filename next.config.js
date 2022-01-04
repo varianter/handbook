48aee9d0712204bc0b78cc828b82c04d42e8cc2c
Mikael Brevik <mikaelbre@gmail.com>
2022-01-04 12:16:28 +0100

    chore: updates to latest next (#156)

--- next.config.js
+++ next.config.js
@@ -1,36 +1,26 @@
 const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
 
-const regexEqual = (x, y) => {
-  return (
-    x instanceof RegExp &&
-    y instanceof RegExp &&
-    x.source === y.source &&
-    x.global === y.global &&
-    x.ignoreCase === y.ignoreCase &&
-    x.multiline === y.multiline
-  );
-};
-
 module.exports = withPlugins([withImages], {
-  target: "serverless",
+  images: {
+    disableStaticImages: true,
+  },
   redirects: async () => {
     return [
       {
-        source: "/:slug*.html",  // Old url with .html
+        source: "/:slug*.html", // Old url with .html
         destination: "/:slug*", // Redirect without .html
-        permanent: true
-      }
-    ]
+        permanent: true,
+      },
+    ];
   },
   webpack: (config) => {
     const oneOf = config.module.rules.find(
       (rule) => typeof rule.oneOf === "object"
     );
     if (oneOf) {
-      const moduleCssRule = oneOf.oneOf.find(
-        (rule) => regexEqual(rule.test, /\.module\.css$/)
-        // regexEqual(rule.test, /\.module\.(scss|sass)$/)
+      const moduleCssRule = oneOf.oneOf.find((rule) =>
+        regexEqual(rule.test, /\.module\.css$/)
       );
       if (moduleCssRule) {
         const cssLoader = moduleCssRule.use.find(({ loader }) =>
@@ -44,3 +34,14 @@ module.exports = withPlugins([withImages], {
     return config;
   },
 });
+
+function regexEqual(x, y) {
+  return (
+    x instanceof RegExp &&
+    y instanceof RegExp &&
+    x.source === y.source &&
+    x.global === y.global &&
+    x.ignoreCase === y.ignoreCase &&
+    x.multiline === y.multiline
+  );
+}
