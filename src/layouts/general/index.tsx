ea218a991d46010970f6a534e03e27b96f5ebb1f
TrymVei <tve@variant.no>
2022-11-29 16:13:58 +0100

    fix: navbar and header are now overlapping

--- src/layouts/general/index.tsx
+++ src/layouts/general/index.tsx
@@ -12,7 +12,6 @@ import { and } from 'src/utils/css';
 import { LayoutProps } from '../signature';
 import style from './layout.module.css';
 import navBackground from './navBackground.svg';
-import backArrow from './backArrow.svg';
 import NavbarLinks from 'src/components/navbarLinks/navbarLinks';
 
 import backArrow from './backArrow.svg';
@@ -284,12 +283,6 @@ export default function GeneralLayout({
 
             {subHeadings.length > 0 ? (
               <>
-                <div className={style.backToVariant}>
-                  <img src={backArrow} alt="Pil tilbake til variant" />
-                  <Link href={'/'}>
-                    <a>Til Variant.no</a>
-                  </Link>
-                </div>
                 <ul>
                   {subHeadings.map((heading) => {
                     return (
