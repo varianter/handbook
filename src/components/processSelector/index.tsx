fd62799c4fb3191e19c96d726d367222e6b83095
Trym Veiby <110395932+TrymVei@users.noreply.github.com>
2022-12-23 11:57:43 +0100

    added component for selecting different sections in process (#247)

---
+++ src/components/processSelector/index.tsx
@@ -0,0 +1,57 @@
+import Link from 'next/link';
+import { NextRouter, useRouter } from 'next/router';
+import { and } from 'src/utils/css';
+import style from './processSelector.module.css';
+
+type Process = {
+  section: string;
+  href: string;
+};
+
+type ProcessSelectorProps = {
+  sections: Process[];
+};
+
+export const ProcessSelector = ({ sections }: ProcessSelectorProps) => {
+  return (
+    <div className={style.processSelector}>
+      {sections.map((section) => {
+        return (
+          <ProcessSelectorButton
+            section={section.section}
+            hrefPath={section.href}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+type ProcessSelectorButtonProps = {
+  section: string;
+  hrefPath: string;
+};
+
+const ProcessSelectorButton = ({
+  section,
+  hrefPath,
+}: ProcessSelectorButtonProps) => {
+  const route = useRouter();
+  return (
+    <div
+      key={section}
+      className={and(
+        style.processSelectorButton,
+        isCurrentPath(hrefPath, route) ? style.processSelectorButtonActive : '',
+      )}
+    >
+      <Link href={hrefPath}>
+        <a>{section}</a>
+      </Link>
+    </div>
+  );
+};
+
+function isCurrentPath(buttonPath: string, route: NextRouter) {
+  return buttonPath.slice(1) === route.asPath;
+}
