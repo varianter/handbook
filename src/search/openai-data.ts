75e1a40aa8042e0c9c13c488eebc77a12b7a78e2
Christian Brevik <christian.brevik@gmail.com>
2023-06-08 09:45:06 +0200

    feat: Move handbook GPT-search and embeddings to our Azure OpenAI (#299)

--- src/search/openai-data.ts
+++ src/search/openai-data.ts
@@ -1,27 +1,25 @@
-import { Configuration, CreateCompletionRequest, OpenAIApi } from 'openai';
+import {
+  ChatCompletionRequestMessage,
+  Configuration,
+  CreateChatCompletionRequest,
+  OpenAIApi,
+} from 'openai';
 import {
   PineconeClient,
   QueryRequest,
   ScoredVector,
 } from '@pinecone-database/pinecone';
 import GPT3Tokenizer from 'gpt3-tokenizer';
-import { codeBlock, oneLine } from 'common-tags';
 
 const config = {
   indexName: 'handbook-index',
   namespace: 'handbook-namespace',
   embeddingModel: 'text-embedding-ada-002',
-  queryModel: 'text-davinci-003',
+  queryModel: 'gpt-35-turbo',
 };
 
 export async function queryOpenai(query: string): Promise<string[]> {
   const openai = await createOpenAIClient();
-  // Moderate the content to comply with OpenAI T&C
-  const moderationResponse = await openai.createModeration({ input: query });
-  const [results] = moderationResponse.data.results;
-  if (results.flagged) {
-    throw new Error("Doesn't comply with OpenAI T&C");
-  }
 
   const embeddingResponse = await openai.createEmbedding({
     model: config.embeddingModel,
@@ -40,51 +38,42 @@ export async function queryOpenai(query: string): Promise<string[]> {
     scoredHandbookItems.map((s) => s.metadata),
   );
 
-  const contextText = getContextText(uniqueMetadatas);
+  const assistentTexts: ChatCompletionRequestMessage[] =
+    getAssistantTexts(uniqueMetadatas);
 
-  const prompt = getPrompt(query, contextText);
+  const messages: ChatCompletionRequestMessage[] = [
+    {
+      role: 'system',
+      content:
+        'Du er en veldig entusiastisk Variant-representant som elsker å hjelpe mennesker! Gitt følgende fra Variant-håndboken (sendt inn som assistent), svar på spørsmålet ved å bruke bare den informasjonen. Hvis du er usikker og svaret ikke er skrevet i håndboken, si "Beklager, jeg vet ikke hvordan jeg kan hjelpe med det." Vennligst ikke skriv URL-er som du ikke finner i kontekstseksjonen.',
+    },
+    ...assistentTexts,
+    {
+      role: 'user',
+      content: query,
+    },
+  ];
 
-  const completionOptions: CreateCompletionRequest = {
+  const completionOptions: CreateChatCompletionRequest = {
     model: config.queryModel,
-    prompt,
+    messages,
     max_tokens: 512,
     temperature: 0,
     stream: false,
   };
-  const res = await openai.createCompletion(completionOptions);
+
+  const res = await openai.createChatCompletion(completionOptions);
   const { choices } = res.data;
-  return choices.map((c) => c.text).filter(Boolean) as string[];
+  return choices.map((c) => c.message?.content).filter(Boolean) as string[];
 }
 
-function getPrompt(query: string, contextText: string) {
-  const prompt = codeBlock`
-  ${oneLine`
-    Du er en veldig entusiastisk Variant-representant som elsker
-    å hjelpe mennesker! Gitt følgende seksjoner fra Variant-håndboken,
-    svar på spørsmålet ved å bruke bare den informasjonen,
-    utgitt i markdown-format. Hvis du er usikker og svaret
-    ikke er skrevet i dokumentasjonen, si
-    "Beklager, jeg vet ikke hvordan jeg kan hjelpe med det."
-    Vennligst ikke skriv URL-er som du ikke finner i kontekstseksjonen.
-
-    Vennligst svar på språket som blir spurt på, mest sannsynlig norsk. Kontekstseksjonene er også på norsk.
-  `}
-
-  Kontekstseksjon:
-  ${contextText}
-
-  Spørsmål: """
-  ${query}
-  """
-`;
-
-  return prompt;
-}
-
-function getContextText(metadataArr: HandbookItemMetadata[]) {
+function getAssistantTexts(
+  metadataArr: HandbookItemMetadata[],
+): ChatCompletionRequestMessage[] {
   const tokenizer = new GPT3Tokenizer({ type: 'gpt3' });
   let tokenCount = 0;
-  let contextText = '';
+
+  let assistantTexts: ChatCompletionRequestMessage[] = [];
   for (let { title, fullContent } of metadataArr) {
     const text = title + ' - ' + fullContent.replace(/\n/g, ' ');
     const encoded = tokenizer.encode(text);
@@ -94,18 +83,30 @@ function getContextText(metadataArr: HandbookItemMetadata[]) {
       break;
     }
 
-    contextText += `${text.trim()}\n---\n`;
+    assistantTexts.push({
+      role: 'assistant',
+      content: text,
+    });
   }
-  return contextText;
+  return assistantTexts;
 }
 
 async function createOpenAIClient() {
-  const apiKey = process.env.OPENAI_API_KEY;
+  const apiKey = process.env.AZURE_OPENAI_API_KEY;
   if (!apiKey)
-    throw new Error('Please set the OPENAI_API_KEY environment variable');
+    throw new Error('Please set the AZURE_OPENAI_API_KEY environment variable');
+
+  const endpoint = process.env.AZURE_OPENAI_ENDPOINT;
+  if (!endpoint)
+    throw new Error(
+      'Please set the AZURE_OPENAI_ENDPOINT environment variable',
+    );
 
   const configuration = new Configuration({
-    apiKey,
+    azure: {
+      apiKey,
+      endpoint,
+    },
   });
 
   return new OpenAIApi(configuration);
@@ -164,11 +165,12 @@ async function queryHandbookIndex(
   const index = pinecone.Index(config.indexName);
   const queryRequest: QueryRequest = {
     vector: queryEmbedding,
-    topK: 5,
+    topK: 3,
     includeValues: false,
     includeMetadata: true,
     namespace: config.namespace,
   };
+
   const queryResponse = await index.query({ queryRequest });
 
   if (!queryResponse?.matches?.length) {
