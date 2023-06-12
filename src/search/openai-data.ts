1003c5e15968705e258e760d9efa57c12f53e2bd
Christian Brevik <christian.brevik@gmail.com>
2023-06-12 12:35:41 +0200

    fix: Better context/question demarcation

--- src/search/openai-data.ts
+++ src/search/openai-data.ts
@@ -38,19 +38,25 @@ export async function queryOpenai(query: string): Promise<string[]> {
     scoredHandbookItems.map((s) => s.metadata),
   );
 
-  const contextTexts: ChatCompletionRequestMessage[] =
-    getContextTexts(uniqueMetadatas);
+  const contextTexts = getContextTexts(uniqueMetadatas);
 
   const messages: ChatCompletionRequestMessage[] = [
     {
       role: 'system',
       content:
-        "Du er en intelligent assistent designet for å hjelpe brukere med å finne informasjon i Variant-håndboken. All informasjon du trenger er sendt i meldingene nedenfor hvor det står 'Fra håndboken:' foran. Du må bare bruke denne informasjonen for å svare på brukerens spørsmål. Hvis spørsmålet ikke er relatert til noe fra håndboken, skal du ikke prøve å svare på det, men si 'Beklager, jeg vet ikke hvordan jeg kan hjelpe med det'. Assistentmeldingene nedenfor er fra Variant-håndboken, og de er på norsk. Brukeren kan stille spørsmål på norsk eller engelsk. Veldig viktig: Hvis du ikke finner svaret i meldingene nedenfor, og er usikker, så må du ikke prøve å svare på spørsmålet, men heller si 'Beklager, jeg vet ikke hvordan jeg kan hjelpe med det'",
+        "Du er en intelligent assistent som skal hjelpe brukere med å finne informasjon i Variant-håndboken. All informasjon du trenger er sendt i meldingene nedenfor hvor utdrag fra håndboken er innenfor <handbook>-tags. Du må bare bruke denne informasjonen til å svare på brukerens spørsmål, som er innenfor <question>-tags. Hvis spørsmålet ikke er relatert til noe fra håndboken, skal du ikke prøve å svare på det, men si 'Beklager, jeg vet ikke hvordan jeg kan hjelpe med det'. Brukeren kan stille spørsmål på norsk eller engelsk, svar på samme språk. Veldig viktig: Hvis du ikke finner svaret i meldingene nedenfor, og er usikker, så må du ikke prøve å svare på spørsmålet, men heller si 'Beklager, jeg vet ikke hvordan jeg kan hjelpe med det'",
     },
-    ...contextTexts,
     {
       role: 'user',
-      content: `Spørsmål: ${query}`,
+      content: `<handbook>Våre grunnverdier - Verdiene våre ligger til grunn for hvordan vi behandler hverandre. Og disse verdiene er: Åpenhet, læreglede og raushet.</handbook> <question>Hva er Variants grunnverdier?</question>`,
+    },
+    {
+      role: 'assistant',
+      content: 'Variants grunnverdier er åpenhet, læreglede og raushet.',
+    },
+    {
+      role: 'user',
+      content: `${contextTexts.join('\n')} <question>${query}</question>`,
     },
   ];
 
@@ -67,13 +73,11 @@ export async function queryOpenai(query: string): Promise<string[]> {
   return choices.map((c) => c.message?.content).filter(Boolean) as string[];
 }
 
-function getContextTexts(
-  metadataArr: HandbookItemMetadata[],
-): ChatCompletionRequestMessage[] {
+function getContextTexts(metadataArr: HandbookItemMetadata[]): string[] {
   const tokenizer = new GPT3Tokenizer({ type: 'gpt3' });
   let tokenCount = 0;
 
-  let contextTexts: ChatCompletionRequestMessage[] = [];
+  let contextTexts: string[] = [];
   for (let { title, fullContent } of metadataArr) {
     const text = title + ' - ' + fullContent.replace(/\n/g, ' ');
     const encoded = tokenizer.encode(text);
@@ -83,10 +87,7 @@ function getContextTexts(
       break;
     }
 
-    contextTexts.push({
-      role: 'user',
-      content: `Fra håndboken: ${text}`,
-    });
+    contextTexts.push(`<handbook>${text}</handbook>`);
   }
   return contextTexts;
 }
