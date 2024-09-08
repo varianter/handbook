8ba9af235ba9fbf2568b2643c35f0cb5241f07b8
Kristoffer Nordström <totte.nordstrom@gmail.com>
2024-09-08 11:08:20 +0200

    feat(oslo): dry oslo ledelse

---
+++ src/components/avdelninger/OsloLedelse.tsx
@@ -0,0 +1,9 @@
+export const OsloLedelse = () => {
+    return (<ul>
+        <li>CEO: Linn</li>
+        <li>CDO: Lars</li>
+        <li>CTO: Nikolai</li>
+        <li>CGO: Even</li>
+        <li>CCO: Arash</li>
+    </ul>)
+};
