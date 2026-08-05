7ee44c5ceee0737dd7b33a96f8cd2f64cbfb0f7e
Jacob Berglund <jacob@jberglund.se>
2026-08-05 12:43:22 +0200

    Removed mdx and updated Astro to version 7.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import { pagefindDev } from './src/integrations/pagefind-dev.ts';
+import { defineConfig } from "astro/config";
+import { pagefindDev } from "./src/integrations/pagefind-dev.ts";
 
 export default defineConfig({
-  integrations: [mdx(), pagefindDev()],
-  redirects: {
-    '/avdelinger': '/avdelinger/trondheim',
-  },
-  vite: {
-    resolve: {
-      alias: {
-        src: '/src',
-      },
-    },
-  },
+	integrations: [pagefindDev()],
+	redirects: {
+		"/avdelinger": "/avdelinger/trondheim",
+	},
+	vite: {
+		resolve: {
+			alias: {
+				src: "/src",
+			},
+		},
+	},
 });
