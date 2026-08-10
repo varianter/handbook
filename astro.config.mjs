206692d569d52bb74bf398efb57a4d58884eedfd
Kristoffer Nordström <totte.nordstrom@gmail.com>
2026-08-10 13:12:35 +0200

    feat: add sitemap
    fixes #204

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
+import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import pagefind from "./src/integrations/pagefind.ts";
 
 export default defineConfig({
-  integrations: [pagefind()],
+  integrations: [pagefind(), sitemap()],
   redirects: {
     "/avdelinger": "/avdelinger/trondheim",
   },
+  site: "https://handbook.variant.no",
   vite: {
     resolve: {
       alias: {
